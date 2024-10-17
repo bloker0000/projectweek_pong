04c039a9975b3ffda92e31ab76d1904b64137089
YlowKnight <58777703+bloker0000@users.noreply.github.com>
2024-10-17 20:59:04 +0200

    Add files via upload

---
+++ site/js/slideshow.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const urls = [
+        'game_page.html',
+        'testpage1.html',
+        'testpage2.html'
+    ];
+
+    //index van de pagina
+    let currentIndex = parseInt(localStorage.getItem('currentIndex')) || 0;
+
+    //functie voor switchen naar paginas
+    const switchPage = (index) => {
+        currentIndex = (index + urls.length) % urls.length;
+        localStorage.setItem('currentIndex', currentIndex);
+        window.location.href = urls[currentIndex];
+    };
+
+    //cooldown van switchen naar pag
+    setInterval(() => {
+        switchPage(currentIndex + 1);
+    }, 15000);
+
+    //shortcut voor switchen naar paginas
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowRight') {
+            switchPage(currentIndex + 1);
+        } else if (event.key === 'ArrowLeft') {
+            switchPage(currentIndex - 1);
+        }
+    });
+});
